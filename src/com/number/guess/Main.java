636ea471a761b7abb50f5d266d491525d46f35e1
Valentina <valelore98@gmail.com>
2022-10-06 14:49:01 +0200

    Implemented a while loop to make the user guess a number

--- src/com/number/guess/Main.java
+++ src/com/number/guess/Main.java
@@ -1,5 +1,28 @@
 package com.number.guess;
 
-public class Main {
+import java.util.Random;
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Random r = new Random();
+		Scanner sc = new Scanner(System.in);
+		boolean guessed = false;
+		int numToGuess = r.nextInt(10);
+		
+		while(!guessed) {
+			System.out.println("Indovina il numero che ho pensato!");
+			int attempt = sc.nextInt();
+			if(attempt != numToGuess) {
+				if(attempt < numToGuess) {
+					System.out.println("Sbagliato! Il tuo numero è troppo piccolo");
+				}else {
+					System.out.println("Sbagliato! Il tuo numero è troppo grande");
+				}
+			}else {
+				guessed = true;
+				System.out.println("Wow hai indovinato!!");
+			}
+		}
+	}
 }
